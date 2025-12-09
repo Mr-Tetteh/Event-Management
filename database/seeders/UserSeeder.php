92a9d587011f601ca28e32cb32df82ae70512566
Mr.Tetteh <danielstay73@gmail.com>
2025-12-09 21:04:18 +0000

    first commit

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'first_name' => 'Daniel',
+            'last_name' => 'Tetteh',
+            'role' => 'suoperadmin',
+            'contact' => '0559724772',
+            'email' => 'danielstay73@gmail.com',
+            'password' => Hash::make('eventmanagement1234?'),
+            'created_at' => now(),
+            'updated_at' => now(),
+            
+        ]);    }
+}

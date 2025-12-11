8e78a0b8f2287a3f02b379f8a518e88029d2e5db
Mr.Tetteh <danielstay73@gmail.com>
2025-12-11 00:44:45 +0000

    sms int

--- routes/admin.php
+++ routes/admin.php
@@ -9,8 +9,18 @@ Route::middleware(['auth'])->group(function () {
         Route::get('beneficiaries', \App\Livewire\Admin\Beneficiary::class)->name('beneficiaries');
         Route::get('funeral-donations', \App\Livewire\Admin\FuneralDonation::class)->name('funeral-donations');
         Route::get('funeral-donation-cash-flow', \App\Livewire\Admin\FuneralDonationCashFlow::class)->name('funeral-donation-cash-flow');
-        Route::get('funeral-donation-cash-flow', \App\Livewire\Admin\FuneralDonationCashFlow::class)->name('funeral-donation-cash-flow');
         Route::get('event-types', \App\Livewire\Admin\EventType::class)->name('event-types')->middleware(\App\Http\Middleware\EnsureTokenIsSuperAdmin::class);
     });
+
+
+    
+
+    // Route::get('sms', function () {
+    //    $response =  sendWithSMSONLINEGH('233559724772', "Dear  thank you for your generous donation of GHS o support our funeral services. Your contribution is greatly appreciated. - Swift Care"); 
+    //    return response()->json([
+    //        'response' => $response,
+    //    ]);
+    // });
+
 
 });

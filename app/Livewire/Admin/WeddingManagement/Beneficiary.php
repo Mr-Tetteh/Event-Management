48a3f6538a5927e552a76223c83aef50ea64a1d5
Mr.Tetteh <danielstay73@gmail.com>
2026-01-21 10:21:14 +0000

    wedding beneficiary

---
+++ app/Livewire/Admin/WeddingManagement/Beneficiary.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Livewire\Admin\WeddingManagement;
+
+use Livewire\Component;
+
+class Beneficiary extends Component
+{
+
+    public $Edit = false;
+    public $beneficiary_id;
+    public $full_name;
+    public $phone;
+
+    public function resetInputFields()
+    {
+        $this->full_name = '';
+        $this->phone = '';
+    }
+    
+
+protected $rules = [
+ 'full_name' => 'required',
+ 'phone' => 'nullable|digits:10',
+];
+ protected $messages = [
+      'full_name.required' => 'Beneficiary full name is required.',
+      'phone.digits' => 'Contact number must be 10 digits.',
+ ];
+
+ public function create()
+{
+    $this->validate();
+
+    \App\Models\WeddingBeneficiary::create([
+        'full_name' => $this->full_name,
+        'phone' => $this->phone,
+    ]);
+
+    $this->dispatch(event: 'toast', message: 'Beneficiary Created Successfully.');
+
+    $this->resetInputFields();
+}
+
+
+public function edit($id){
+    $this->Edit = true;
+    $beneficiary = \App\Models\WeddingBeneficiary::findOrFail($id);
+    $this->beneficiary_id = $id;
+    $this->full_name = $beneficiary->full_name;
+    $this->phone = $beneficiary->phone;
+}
+
+public function update()
+{
+    $this->validate();
+    
+    $beneficiary = \App\Models\WeddingBeneficiary::find($this->beneficiary_id);
+    $beneficiary->update([
+        'full_name' => $this->full_name,
+        'phone' => $this->phone,
+    ]);
+    $this->dispatch(event: 'toast', message: 'Beneficiary Updated Successfully.');
+
+    $this->resetInputFields();
+    $this->Edit = false;
+}
+
+public function delete($id){
+    \App\Models\WeddingBeneficiary::find($id)->delete();
+    $this->dispatch(event: 'toast', message: 'Beneficiary Deleted Successfully.');
+
+}
+    public function render()
+    {
+        $datas = \App\Models\WeddingBeneficiary::all();
+        return view('livewire.admin.wedding-management.beneficiary', compact('datas'));
+    }
+}
